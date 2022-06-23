47710a6658f2970d0b70158f1d0c355efda72925
halisycel <isaycel2001@gmail.com>
2022-06-23 14:41:23 +0300

    edit bubble-sort

--- algorithms/bubble-sort/bubble-sort.js
+++ algorithms/bubble-sort/bubble-sort.js
@@ -3,14 +3,13 @@ const bubbleSort = (array) => {
 	let isSorted;
 	for (let i = 0; i < array.length; i++) {
 		isSorted = true;
-		for (let j = 0; j < (array.length - i - 1); j++) {
+		for (let j = 0; j < (array.length - i - 1); j++)
 			if (array[j] > array[j+1]) {
 				isSorted = false;
 				tmp = array[j+1];
 				array[j+1] = array[j];
 				array[j] = tmp;
 			}
-		}
 		if (isSorted) break;
 	}
 }
